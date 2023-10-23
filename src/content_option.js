1d08f57e446d742f9b32ca3953fe8bd4281b8838
Mogreen99 <Mogreen882@gmail.com>
2023-10-23 14:14:52 +0100

    first commit

---
+++ src/content_option.js
@@ -0,0 +1,164 @@
+const logotext = "Welcome";
+const meta = {
+    title: "Mo Green",
+    description: "I’m Mo Green Web Developer _ Frontend devloper,currently working in London",
+};
+
+const introdata = {
+    title: "Hello, I’m Mo Green",
+    animated: {
+        first: "Frontend Developer",
+        second: "React Engineer",
+        third: "Web Applications Developer",
+        forth: "Welcome To My Page..",
+    },
+    description: "I am a junior Frontend Developer. I have been part of teams that worked on a full-lifecycle projects and I have been exposed to many parts of the projects including Backend, developed with Node.js and Java. I mainly worked on the Frontend using HTML, CSS and JavaScript specializing in React.",
+    your_img_url: "https://images.unsplash.com/photo-1628258334105-2a0b3d6efee1",
+};
+
+const dataabout = {
+    title: "A bit about my self",
+    aboutme: "With just over a year of commercial software development experience, I have been part of teams that worked on full-lifecycle projects. I have been exposed to many parts of the projects including Backend developed with Node.js and Java. I mainly worked on the Frontend using HTML, CSS and JavaScript specializing in React. I have also deployed web applications on application servers running Linux platform. I followed the DevOps culture and Agile project management.",
+};
+const worktimeline = [{
+        jobtitle: "Designer of week",
+        where: "YAdfi",
+        date: "2020",
+    },
+    {
+        jobtitle: "Designer of week",
+        where: "Jamalya",
+        date: "2019",
+    },
+    {
+        jobtitle: "Designer of week",
+        where: "ALquds",
+        date: "2019",
+    },
+];
+
+const skills = [{
+        name: "Javascript",
+        value: 90,
+    },
+    {
+        name: "React",
+        value: 80,
+    },
+    {
+        name: "Typescript",
+        value: 70,
+    },
+    {
+        name: "Terminal",
+        value: 70,
+    },
+    {
+        name: "GitHub",
+        value: 80,
+    },
+];
+
+const experience = [{
+        title: "Jikolux POS App",
+        description: "A point-of-sale system that was developed for Restaurants and similar businesses, which enables them to manage their daily operations, such as orders, sales payments and expenses, staff Registrations and log ins etc.",
+    },
+    {
+        title: "Ministry of Health Somalia",
+        description: "Developed a Covid19 Passport system, which is managed by the MOH giving access to all Health Centres in the country to record Covid vaccinations and tests. Also manage appointments. The system was developed with Java (Spring Boot) Backend, React Frontend.",
+    },
+
+];
+
+const dataportfolio = [{
+        img: "https://picsum.photos/400/?grayscale",
+        description: "The wisdom of life consists in the elimination of non-essentials.",
+        link: "#",
+    },
+    {
+        img: "https://picsum.photos/400/800/?grayscale",
+        description: "The wisdom of life consists in the elimination of non-essentials.",
+        link: "#",
+    },
+    {
+        img: "https://picsum.photos/400/?grayscale",
+        description: "The wisdom of life consists in the elimination of non-essentials.",
+        link: "#",
+    },
+    {
+        img: "https://picsum.photos/400/600/?grayscale",
+        description: "The wisdom of life consists in the elimination of non-essentials.",
+        link: "#",
+    },
+    {
+        img: "https://picsum.photos/400/300/?grayscale",
+        description: "The wisdom of life consists in the elimination of non-essentials.",
+        link: "#",
+    },
+    {
+        img: "https://picsum.photos/400/700/?grayscale",
+        description: "The wisdom of life consists in the elimination of non-essentials.",
+        link: "#",
+    },
+
+    {
+        img: "https://picsum.photos/400/600/?grayscale",
+        description: "The wisdom of life consists in the elimination of non-essentials.",
+        link: "#",
+    },
+    {
+        img: "https://picsum.photos/400/300/?grayscale",
+        description: "The wisdom of life consists in the elimination of non-essentials.",
+        link: "#",
+    },
+    {
+        img: "https://picsum.photos/400/?grayscale",
+        description: "The wisdom of life consists in the elimination of non-essentials.",
+        link: "#",
+    },
+    {
+        img: "https://picsum.photos/400/550/?grayscale",
+        description: "The wisdom of life consists in the elimination of non-essentials.",
+        link: "#",
+    },
+    {
+        img: "https://picsum.photos/400/?grayscale",
+        description: "The wisdom of life consists in the elimination of non-essentials.",
+        link: "#",
+    },
+    {
+        img: "https://picsum.photos/400/700/?grayscale",
+        description: "The wisdom of life consists in the elimination of non-essentials.",
+        link: "#",
+    },
+];
+
+const contactConfig = {
+    YOUR_EMAIL: "Mogreen882@gmail.com",
+    YOUR_FONE: "(+44)07594305696",
+    description: "Please, feel free to contact me at any time, also on the left side you can find all my socials and platforms.. ",
+    // creat an emailjs.com account 
+    // check out this tutorial https://www.emailjs.com/docs/examples/reactjs/
+    YOUR_SERVICE_ID: "service_id",
+    YOUR_TEMPLATE_ID: "template_id",
+    YOUR_USER_ID: "user_id",
+};
+
+const socialprofils = {
+    github: "https://github.com/MOGREEN99",
+    linkedin: "https://www.linkedin.com/in/mohamed-mohamed-941431233/",
+    facebook: "https://www.facebook.com/MohamedGreeno/",
+    twitter: "https://twitter.com/Mogreeno99",
+};
+export {
+    meta,
+    dataabout,
+    dataportfolio,
+    worktimeline,
+    skills,
+    experience,
+    introdata,
+    contactConfig,
+    socialprofils,
+    logotext,
+};
